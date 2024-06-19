79fe7327a8bb9626827b8e672afd9a1708f51aa2
Juan Manuel Otero <Juan_m_o@hotmail.com>
2024-06-19 15:27:09 -0300

    Se agrego el loguin de  usuarios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
-    path('loguin/', views.loguin, name='loguin '),
+    path('loguin/', views.loguin, name='loguin'),
+    path('logout/', LogoutView.as_view(template_name='usuarios/logout.html'), name='logout'),
+    path('registro/', views.registro, name='registro')
    
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import AuthenticationForm 
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from usuarios.forms import Formulariocreate
 # Create your views here.
 def loguin (request):
     formulario= AuthenticationForm()
+    if request.method=='POST':
+        formulario= AuthenticationForm(request,data=request.POST)
+        if formulario.is_valid():
+           usuario=formulario.cleaned_data.get('username')
+           contrasenia=formulario.cleaned_data.get('password')
+           
+           user=authenticate(request, username=usuario, password=contrasenia )
+           login(request, user)
+           
+           return redirect('inicio')
     
+    return render(request, 'usuarios/loguin.html', {'formulario':formulario})
+
+def registro(request):
+    formulario= Formulariocreate()
+    if request.method=='POST':
+        formulario= Formulariocreate(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            
+            return redirect('loguin')
+        
     
-    return render(request, 'usuarios/loguin.html', {})
+    return render(request, 'usuarios/registro.html', {'formulario':formulario})
